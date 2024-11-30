b99b39fdc554b2fd1400a973562729dad1298140
Your NameRishi <rishirg312@gmail.com>
2024-12-01 00:16:16 +0530

    clear

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,19 +1,7 @@
-// import Parent from "./Components/Parent";
-import Datatable from "./Components/Datatable";
-import UseEffectHook from "./Components/UseEffectHook";
-
 const App = () => {
-
-    const arr1 = [
-        {name:"Name1",  designation:"designation1"},
-        {name:"Name2",  designation:"designation2"},
-        {name:"Name3",  designation:"designation3"},
-    ];
 
     return (
             <div className="app">
-                <Datatable data={arr1} />
-                <UseEffectHook />
             </div>
     );
 };
